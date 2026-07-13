678c4064fa35b481d3f1b5fae2a3ab99ac60273e
Jac LL <210639514+J-Yhwh@users.noreply.github.com>
2026-07-13 18:58:49 +0800

    Add files via upload
    Signed-off-by: Jac LL <210639514+J-Yhwh@users.noreply.github.com>

---
+++ scripts/pyspark_processor.py
@@ -0,0 +1,15 @@
+
+from pyspark.sql import SparkSession
+import pandas as pd
+
+
+#collate csv masterlist from src scripts to prepare for processing
+
+def process_with_pyspark(data_path):
+    spark = SparkSession.builder.appName("CookieAnalysis").getOrCreate()
+    df = spark.read.csv(data_path, header=True, inferSchema=True)
+
+    #Example processing
+    
+    aggregated = df.groupby("domain").count()
+    return aggregated.toPandas()      #Convert to pandas for Streamlit
